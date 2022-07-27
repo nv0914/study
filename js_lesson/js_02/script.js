b19bee5f88aedb773f5623fd6aae249d1007ecda
Nazarii <nazar.volitskii@gmail.com>
2022-07-27 13:23:53 +0300

    nan

---
+++ js_lesson/js_02/script.js
@@ -0,0 +1,113 @@
+let arrOne = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log(arrOne[0]);
+console.log(arrOne[1]);
+console.log(arrOne[2]);
+console.log(arrOne[3]);
+console.log(arrOne[4]);
+console.log(arrOne[5]);
+console.log(arrOne[6]);
+console.log(arrOne[7]);
+console.log(arrOne[8]);
+console.log(arrOne[9]);
+
+let bookOne = {
+    title: 'The Graveyard',
+    pageCount: 250,
+    genre: 'Comedy'
+}
+let bookTwo = {
+    title: 'Bleach',
+    pageCount: 880,
+    genre: 'Adventure'
+}
+let bookThree = {
+    title: 'Under the Dome',
+    pageCount: 990,
+    genre: 'Drama'
+}
+let bookFour = {
+    title: 'Under the Home',
+    pageCount: 990,
+    genre: 'Drama',
+    authors: ['S. Prince', 90]
+}
+let bookFive = {
+    title: 'Middle of Nowhere',
+    pageCount: 88,
+    genre: 'Fantasy',
+    authors: ['C.P. Rolling', 55]
+}
+let bookSix = {
+    title: 'Harry Daughter',
+    pageCount: 87,
+    genre: 'Drama',
+    authors: ['M.J. Parker', 84]
+}
+
+let users = [{
+    name: 'John Smith',
+    username: 'FallenAngel007',
+    password: 'vbi78uy09y8oih'
+}
+    , {
+        name: 'Andrew Wallace',
+        username: 'Wallella',
+        password: '87iuhkj'
+    },
+    {
+        name: 'Julius Murphy',
+        username: 'JayEm',
+        password: '76tyiughj'
+    },
+    {
+        name: 'Matthew Lewis',
+        username: 'Biggish_Matthew',
+        password: 'nkljhiuoy890opj'
+    },
+    {
+        name: 'Marc Martin',
+        username: 'Slippery_Muffin',
+        password: 'jliu8oy97i'
+    },
+    {
+        name: 'Emelie Clark',
+        username: 'Bang-up-Emelie',
+        password: '9y7h7y8'
+    },
+    {
+        name: 'Johnny Johnston',
+        username: 'Giovanni',
+        password: 'u76t87g'
+    },
+    {
+        name: 'Wilhelmina Marshall',
+        username: 'Wilhelminakitten',
+        password: '98y9uib7'
+    },
+    {
+        name: 'Paulina Burt Shaw',
+        username: 'Peachy_Paulina',
+        password: '946g6df196d'
+    },
+    {
+        name: 'Antoinette Crawford',
+        username: 'Fluffy_Antoinette',
+        password: 'f15f1'
+    },
+    {
+        name: 'Donna Stewart',
+        username: 'Smelly-Muffin',
+        password: 'f4b6fb1'
+    }
+]
+console.log(users[0].password);
+console.log(users[1].password);
+console.log(users[2].password);
+console.log(users[3].password);
+console.log(users[4].password);
+console.log(users[5].password);
+console.log(users[6].password);
+console.log(users[7].password);
+console.log(users[8].password);
+console.log(users[9].password);
+console.log(users[10].password);
