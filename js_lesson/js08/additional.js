30ec8e8ad23718e2e180236c8acd3e046e199659
Nazarii <nazar.volitskii@gmail.com>
2022-08-17 21:34:52 +0300

    classes

--- js_lesson/js08/additional.js
+++ js_lesson/js08/additional.js
@@ -21,6 +21,7 @@ class AdressData extends AdressGeoData {
         }
     }
 }
+
 class PersonalProfile extends AdressData {
     constructor(id, name, username, email, phone, website) {
         super(...profileAdress);
@@ -43,6 +44,7 @@ class PersonCompanyProfile extends PersonalProfile {
         }
     }
 }
+
 let adressGeo = ['51.6001', '-0.0597'],
     profileAdress = ['Foil road', 'h. 27', 'London', 115588],
     profileInfo = [5544, 'Paul Atreides', 'blue_eyed', 'thirsty-man@tail.com', '8-551-987-215', 'dunemovie.net'],
@@ -76,6 +78,9 @@ let megaObj = {
     },
 }
 
+// Нащо? Для чого? Просто грався
+
+
 // -  Створити функцію конструктор / клас  який описує об'єкт тег
 
 // Поля :
@@ -96,6 +101,58 @@ let megaObj = {
 // -form
 // -option
 // -select
+function CreateElementDescription(titleOfTag, action) {
+    this.titleOfTag = titleOfTag
+    this.action = action
+    this.attrs = []
+    this.addAttrs = function (obj) {
+        this.attrs.push(obj)
+    }
+}
+
+function CreateElementAttribute(titleOfAttr, actionOfAttr) {
+    this.titleOfAttr = titleOfAttr
+    this.actionOfAttr = actionOfAttr
+}
+
+let a = new CreateElementDescription('<a>', 'Тег <a> является ' +
+    'одним из важных элементов HTML и предназначен для создания ссылок.',)
+a.addAttrs(new CreateElementAttribute('accesskey', 'Активация ссылки с помощью комбинации клавиш.'))
+a.addAttrs(new CreateElementAttribute('coords', 'Устанавливает координаты активной области.'))
+
+let div = new CreateElementDescription('<div>', 'Элемент <div> является' +
+    ' блочным элементом и предназначен для выделения фрагмента...',)
+div.addAttrs(new CreateElementAttribute('align', 'Задает выравнивание содержимого тега <div>.'))
+div.addAttrs(new CreateElementAttribute('title', 'Добавляет всплывающую подсказку к содержимому.'))
+
+let h1 = new CreateElementDescription('<h1>', 'Тег <h1> представляет собой' +
+    ' наиболее важный заголовок первого уровня',)
+h1.addAttrs(new CreateElementAttribute('align', 'Определяет выравнивание заголовка.'))
+
+let span = new CreateElementDescription('<span>', 'Тег <span> предназначен для определения строчных элементов документа.',)
+span.addAttrs(new CreateElementAttribute('dir', 'Задает направление и отображение текста — слева направо или справа налево.'))
+span.addAttrs(new CreateElementAttribute('hidden', 'Скрывает содержимое элемента от просмотра.'))
+
+let input = new CreateElementDescription('<input>', 'Тег <input> является' +
+    ' одним из разносторонних элементов формы',)
+input.addAttrs(new CreateElementAttribute('accept', 'Устанавливает фильтр на типы файлов, которые вы можете отправить через поле загрузки файлов.'))
+input.addAttrs(new CreateElementAttribute('autofocus', 'Устанавливает фокус в поле формы.'))
+
+let form = new CreateElementDescription('<form>', 'Тег <form> устанавливает' +
+    ' форму на веб-странице. ',)
+form.addAttrs(new CreateElementAttribute('name', 'Имя формы.'))
+form.addAttrs(new CreateElementAttribute('novalidate', 'Отменяет встроенную проверку данных формы на корректность ввода.'))
+
+let option = new CreateElementDescription('<option>', 'Тег <option> определяет отдельные пункты списка, создаваемого с помощью контейнера <select>.',)
+option.addAttrs(new CreateElementAttribute('disabled', 'Заблокировать для доступа элемент списка.'))
+option.addAttrs(new CreateElementAttribute('label', 'Указание метки пункта списка.'))
+
+let select = new CreateElementDescription('<select>', 'Тег <select>' +
+    ' позволяет создать элемент интерфейса в виде раскрывающегося списка',)
+select.addAttrs(new CreateElementAttribute('required', 'Количество отображаемых строк списка.'))
+select.addAttrs(new CreateElementAttribute('tabindex', 'Определяет последовательность перехода между элементами при нажатии на клавишу Tab'))
+
+
 // Приклад результуючого об'єкту
 // {
 //     titleOfTag: 'area',
