f77114042b1c2c67e521ac456793bd13b0eb062d
Nazarii <nazar.volitskii@gmail.com>
2022-09-18 16:51:38 +0300

    lesson_2

--- react/src/App.js
+++ react/src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
-import {CharacterGenerator} from "./components/CharacterGenerator";
+import {UserGenerator} from "./components/lesson_2/UsersGenerator";
+import {SpaceXGenerator} from "./components/lesson_2/SpaceXGenerator";
+
+// import {CharacterGenerator} from "./components/lesson_1/CharacterGenerator";
 
 function App() {
-  return (<div>
-    <CharacterGenerator/>
-      </div>
-  );
+    return (<div>
+            {/*<CharacterGenerator/>*/
+                <SpaceXGenerator/>
+            }
+        </div>
+    );
 }
 
 export default App;
